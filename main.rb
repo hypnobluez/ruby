92e1da5fd12b159e09ec58384fb6902455d730e7
hypnobluez <hypnobluez@gmail.com>
2024-01-27 14:46:12 +1100

    addition function

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
 puts 'hello world'
+
+def sum_of_two_numbers(a, b)
+    a + b
+end
+
+result1 = sum_of_two_numbers(2, 3)
+puts "The sum of 2 and 3 is: #{result1}"
+
+result2 = sum_of_two_numbers(2, 7)
+puts "The sum of 2 and 7 is: #{result2}"
